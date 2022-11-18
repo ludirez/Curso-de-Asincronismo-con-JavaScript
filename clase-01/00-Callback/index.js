d57c9e9fcb7c887a27d6ea39fcb8e252861e83f0
ludirez <diazrestrepo1@gmail.com>
2022-11-17 23:06:53 -0500

    clase Callbanck

---
+++ clase-01/00-Callback/index.js
@@ -0,0 +1,32 @@
+//Es una función que se pasa a otra función como un argumento, que luego se invoca dentro de la función externa para completar algún tipo de rutina o acción
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+function calc(num1, num2, cualquierNombreParaElCallback) {
+    return cualquierNombreParaElCallback(num1, num2)
+}
+
+console.log(calc(2, 2, sum));
+
+//Set Time out
+
+setTimeout(function () {
+    console.log('hola JavaScript');
+}, 2000)
+
+//ejemplo de setTimeout
+function greeting(name) {
+    console.log(`hola ${name}`);
+}
+
+setTimeout(greeting, 2000, 'Res')//otra manera
+
+setTimeout(()=>greeting('Dias'), 2000)//otra manera
+
+setTimeout(() => {//otra manera
+    return greeting('luis');
+}, 2000);
+
+setTimeout(greeting, 2000, 'Alfred') //otra manera
